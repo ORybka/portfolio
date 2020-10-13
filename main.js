067665836c9fec6a4240dc816fa801ab6fa55ab4
ORybka <olha.rybchynska@gmail.com>
2020-10-13 11:17:38 +0300

    adapt vers

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 VARIABLES
 ***********************************************/
 let header = document.querySelector(".menu-container");
-let NavLinks = document.querySelectorAll("nav ul li a");
+let navLinks = document.querySelectorAll("nav ul li a");
+let nav = document.querySelector("nav");
 let mainSections = document.querySelectorAll("body section");
 let text = document.getElementsByClassName("text-footer");
 let footerColors = ["#fff", "#f95c6d", "#ff9e85", "#613b3b", "#49ada4", "#b3e6e0", "#436d6c"];
@@ -53,7 +54,7 @@ NAVBAR - ACTIVE ITEM
 window.addEventListener("scroll", (e) => {
   let fromTop = window.scrollY;
 
-  NavLinks.forEach((link) => {
+  navLinks.forEach((link) => {
     let section = document.querySelector(link.hash);
 
     if (
@@ -68,6 +69,15 @@ window.addEventListener("scroll", (e) => {
 });
 
 /**********************************************
+BURGER MENU
+***********************************************/
+$(document).ready(function() {
+  $(".navbar-burger").click(function(e) {
+    $(".navbar-burger, #nav").toggleClass("active");
+  })
+});
+
+/**********************************************
 FOOTER
 ***********************************************/
 let i = 0;
@@ -78,3 +88,34 @@ function changeColor() {
   });
 }
 setInterval(changeColor, 1000);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  } else {
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "block";
+    }
+  }
+}
